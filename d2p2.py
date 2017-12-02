e6ca556cda5b66de8b1c9c8e858bcff34e674791
balajivenkatesh <mjbalajivenkatesh@gmail.com>
2017-12-02 01:56:27 -0800

    d2 and blog init

--- d2p2.py
+++ d2p2.py
@@ -10,10 +10,15 @@ except Exception as e:
   print type(e), e
   sys.exit(0)
 
+def rowChecksum(row):
+  n = len(row)
+  for i in range(0, n):
+    for j in range(0, n):
+      if row[j] % row[i] == 0 and i != j:
+        return row[j] / row[i]
+
 sum = 0
 for row in a:
-  minX = min(row)
-  maxX = max(row)
-  sum = sum + maxX - minX
+  sum = sum + rowChecksum(row)
 
 print sum
